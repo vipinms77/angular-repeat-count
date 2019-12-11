c3956d7433a0d847ac19bdbbef408c0f1dbeaf1b
vipinms77 <vipinms77@gmail.com>
2019-12-11 17:55:47 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular';
+  numberString;
+  array: number[] = [];
+  integer:number;
+  num: number;
+  onClick(){
+    console.log(this.numberString);
+    if(this.numberString.trim()){
+      this.array = this.numberString.split(',');
+    }
+    if(this.array.length > 0 && this.integer){
+      this.countNum(this.array,this.integer);
+    }
+  }
+  countNum(array:number[], integer:number) {
+    this.num = array.filter(x => x == integer).length;
+    console.log(this.num);
+  }
 }
